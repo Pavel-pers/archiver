d3b8630f15f62f4e836b07bee58336d9befeb0bf
Pavel Turaev <pnturaev@edu.hse.ru>
2024-09-19 14:00:58 +0300

    solve problem

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,6 +1,6 @@
 #include "reduce_fraction.h"
-
+#include <__numeric/gcd_lcm.h>
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    return 1;
+    return (numerator / std::gcd(numerator, denominator)) + (denominator / std::gcd(numerator, denominator));
 }

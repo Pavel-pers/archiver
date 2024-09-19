775cae27d8cbae8ced0a4b3bd4db0801736770e1
Pavel Turaev <pnturaev@edu.hse.ru>
2024-09-19 13:25:26 +0300

    test

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -1,5 +1,12 @@
 #include "min_divisor.h"
 
+const int64_t MIN_NAT_NUMBER = 2;
+
 int64_t MinDivisor(int64_t number) {
-    return 2;
+    for (int64_t d = MIN_NAT_NUMBER; d * d <= number; ++d) {
+        if (number % d == 0) {
+            return d;
+        }
+    }
+    return number;
 }

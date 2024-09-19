1649ab10843096e6f87abf4a1e05e2881f7cda59
Pavel Turaev <pnturaev@edu.hse.ru>
2024-09-19 14:10:33 +0300

    solve problem

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,26 @@
 #include "palindrome.h"
 
+const size_t FIRST_STRING_INDEX = 0;
+
 bool IsPalindrome(const std::string& str) {
-    return false;
+    size_t left_index = FIRST_STRING_INDEX;
+    size_t right_index = str.size() - 1;
+
+    while (left_index < right_index) {
+        if (str[left_index] == ' ') {
+            ++left_index;
+            continue;
+        }
+        if (str[right_index] == ' ') {
+            --right_index;
+            continue;
+        }
+
+        if (str[left_index] == str[right_index]) {
+            ++left_index, --right_index;
+        } else {
+            return false;
+        }
+    }
+    return true;
 }

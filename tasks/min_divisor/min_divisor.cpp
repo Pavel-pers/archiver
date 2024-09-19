c1a96b1cbcd6c57dcce2d7e07e12a6f516a0fe68
Pavel Turaev <pnturaev@edu.hse.ru>
2024-09-19 13:28:06 +0300

    change type of const

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -1,6 +1,6 @@
 #include "min_divisor.h"
 
-const int64_t MIN_NAT_NUMBER = 2;
+const uint64_t MIN_NAT_NUMBER = 2;
 
 int64_t MinDivisor(int64_t number) {
     for (int64_t d = MIN_NAT_NUMBER; d * d <= number; ++d) {

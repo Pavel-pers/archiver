b83336bbfc17df4f6576bbcd9a53eacbb1115976
Pavel Turaev <pnturaev@edu.hse.ru>
2024-09-22 13:48:48 +0300

    change cast to tuple on tie cast

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -6,16 +6,16 @@ void SortStudents(std::vector<Student>& students, SortKind const sort_kind) {
               sort_kind == SortKind::Name
                   ? [](Student left, Student right) {
                       return
-                          std::tuple(left.last_name, left.name,
+                          std::tie(left.last_name, left.name,
                                      left.birth_date.year, left.birth_date.month, left.birth_date.day) <
-                          std::tuple(right.last_name, right.name,
+                          std::tie(right.last_name, right.name,
                                      right.birth_date.year, right.birth_date.month, right.birth_date.day);
                   }
                   : [](Student left, Student right) {
                       return
-                          std::tuple(left.birth_date.year, left.birth_date.month, left.birth_date.day,
+                          std::tie(left.birth_date.year, left.birth_date.month, left.birth_date.day,
                                      left.last_name, left.name) <
-                          std::tuple(right.birth_date.year, right.birth_date.month, right.birth_date.day,
+                          std::tie(right.birth_date.year, right.birth_date.month, right.birth_date.day,
                                      right.last_name, right.name);
                   }
         );

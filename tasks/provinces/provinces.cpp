3d0548daa8b158e42953b95b05de4af0b4f76deb
Pavel Turaev <pnturaev@edu.hse.ru>
2024-10-10 12:26:33 +0300

    solve problem

--- tasks/provinces/provinces.cpp
+++ tasks/provinces/provinces.cpp
@@ -1,5 +1,19 @@
 #include "provinces.h"
 
 int64_t CountPassports(const std::vector<int>& provinces) {
-    return 0;
+    std::multiset<int64_t> ord;
+    for (const int count : provinces) {
+        ord.insert(static_cast<int64_t>(count));
+    }
+
+    int64_t min_cost = 0;
+    while (ord.size() > 1) {
+        int64_t new_size = *(ord.begin()) + *(++ord.begin());
+        min_cost += new_size;
+        ord.erase(ord.begin());
+        ord.erase(ord.begin());
+        ord.insert(new_size);
+    }
+
+    return min_cost;
 }

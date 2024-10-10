5b1df06e4a626fda2ea9ee394cbe0bb6aa67392b
Pavel Turaev <pnturaev@edu.hse.ru>
2024-10-10 15:12:13 +0300

    chage int types

--- tasks/provinces/provinces.cpp
+++ tasks/provinces/provinces.cpp
@@ -1,15 +1,15 @@
 #include "provinces.h"
 #include <cctype>
 
-int64_t CountPassports(const std::vector<int>& provinces) {
-    std::multiset<int64_t> ord;
+__int64_t CountPassports(const std::vector<int>& provinces) {
+    std::multiset<__int64_t> ord;
     for (const int count : provinces) {
-        ord.insert(static_cast<int64_t>(count));
+        ord.insert(static_cast<__int64_t>(count));
     }
 
-    int64_t min_cost = 0;
+    __int64_t min_cost = 0;
     while (ord.size() > 1) {
-        int64_t new_size = *(ord.begin()) + *(++ord.begin());
+        __int64_t new_size = *(ord.begin()) + *(++ord.begin());
         min_cost += new_size;
         ord.erase(ord.begin());
         ord.erase(ord.begin());

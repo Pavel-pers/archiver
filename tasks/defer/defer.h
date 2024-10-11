62579b38059631764c35ba8d70f40c718e1cfff9
Pavel Turaev <pnturaev@edu.hse.ru>
2024-10-12 00:30:56 +0300

    solve problem

--- tasks/defer/defer.h
+++ tasks/defer/defer.h
@@ -1,3 +1,20 @@
 #pragma once
 
-#define defer
+template <typename Lambda>
+class DeferredFunction {
+public:
+    DeferredFunction(Lambda callback) : callback_(callback) {
+    }
+
+    ~DeferredFunction() {
+        callback_();
+    };
+
+private:
+    Lambda callback_;
+};
+
+#define __DEFER_CONCAT_VAR_NAMES(ls, rs) ls##rs
+#define __DEFER_MID_CONCAT(ls, rs) __DEFER_CONCAT_VAR_NAMES(ls, rs)
+#define __DEFER_GEN_NEW_NAME __DEFER_MID_CONCAT(___defer_var_, __COUNTER__)
+#define defer DeferredFunction __DEFER_GEN_NEW_NAME = [&]()

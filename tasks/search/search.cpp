a1381af0c7beb37f978790c80fa85c0936ab36a9
Pavel Turaev <pnturaev@edu.hse.ru>
2024-10-12 13:49:38 +0300

    solve problem

--- tasks/search/search.cpp
+++ tasks/search/search.cpp
@@ -1,5 +1,6 @@
 #include "search.h"
 #include <algorithm>
+#include <numeric>
 #include <cctype>
 #include <cmath>
 #include <unordered_map>
@@ -7,25 +8,30 @@
 
 const double EPS = 1e-7;
 
-class WordHash {
+class HashWord {
 public:
-    __uint32_t operator()(const std::string_view s) const {
-        __uint32_t hash = 0;
-        for (char i : s) {
-            hash = (static_cast<__uint64_t>(hash) * BASE + (tolower(i) - 'a' + 1)) % MOD;
+    uint32_t operator()(const std::string_view s) const {
+        uint32_t result_hash = 0;
+        for (char c : s) {
+            c = static_cast<char>(tolower(c));
+            result_hash = (static_cast<uint64_t>(BASE) * static_cast<uint64_t>(result_hash) + (c - 'a')) % MOD;
         }
-        return hash;
+        return result_hash;
     }
 
 private:
-    static const __uint32_t BASE = 31;
-    static const __uint32_t MOD = 1e9 + 7;
+    static const uint32_t MOD = 1e9 + 7;
+    static const uint32_t BASE = 31;
 };
 
-class EqToIgrnoringCase {
+class CompareWord {
 public:
     bool operator()(const std::string_view lhs, const std::string_view rhs) const {
-        for (int i = 0; i < std::min(lhs.size(), rhs.size()); i++) {
+        if (lhs.size() != rhs.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < lhs.size(); i++) {
             if (tolower(lhs[i]) != tolower(rhs[i])) {
                 return false;
             }
@@ -34,80 +40,84 @@ public:
     }
 };
 
+size_t GetWordLen(std::string_view pref) {
+    for (int d = 0; d < pref.size(); d++) {
+        if (!isalpha(pref[d])) {
+            return d;
+        }
+    }
+    return pref.size();
+}
+
 std::vector<std::string_view> Search(std::string_view text, std::string_view query, size_t results_count) {
     if (text.empty() || query.empty()) {
         return {};
     }
 
-    std::unordered_map<std::string_view, std::vector<std::pair<size_t, size_t>>, WordHash, EqToIgrnoringCase>
-        matches_str;
-    for (size_t start_indx = 0; start_indx < query.size(); ++start_indx) {
-        for (size_t len = 0;; ++len) {
-            if (len + start_indx == query.size() || !std::isalpha(query[start_indx + len])) {
-                if (len) {
-                    matches_str.insert({query.substr(start_indx, len), {}});
-                }
-                start_indx += len;
-                break;
+    std::unordered_map<std::string_view, std::vector<std::pair<size_t, size_t>>, HashWord, CompareWord>
+        word_concurrence;
+    for (size_t l = 0, r = 0; r <= query.size(); r++) {
+        if (r == query.size() || !isalpha(query[r])) {
+            if (l != r) {
+                word_concurrence.insert({query.substr(l, r - l), {}});
             }
+            l = r + 1;
         }
     }
 
-    std::vector<std::pair<double, std::string_view>> ordered_str;
-    std::vector<size_t> count_of_words;
+    std::vector<std::string_view> splited_str;
+    for (size_t char_ind = 0, line_start = 0; char_ind <= text.size(); char_ind++) {
+        if (char_ind == text.size() || text[char_ind] == '\n') {
+            splited_str.push_back(text.substr(line_start, char_ind - line_start));
+            line_start = char_ind + 1;
+        }
+    }
 
-    for (size_t start_indx = 0, start_str = 0, cur_str = 0, word_count = 0; start_indx < text.size(); ++start_indx) {
-        for (int len = 0;; len++) {
-            if (start_indx + len == text.size() || !std::isalpha(text[start_indx + len])) {
-                if (len) {
-                    word_count++;
-                    std::string_view cur_w = text.substr(start_indx, len);
-                    if (matches_str.count(cur_w)) {
-                        auto& matches = matches_str[cur_w];
-                        if ((matches.empty() || matches.back().first != cur_str)) {
-                            matches.push_back({cur_str, 0});
-                        }
-                        matches.back().second++;
+    std::vector<size_t> str_word_count(splited_str.size());
+    for (size_t str_ind = 0; str_ind < splited_str.size(); str_ind++) {
+        std::string_view line = splited_str[str_ind];
+        for (size_t char_ind = 0; char_ind < line.size(); char_ind++) {
+            size_t word_len = GetWordLen(line.substr(char_ind));
+            if (word_len > 0) {
+                str_word_count[str_ind]++;
+                std::string_view word = line.substr(char_ind, word_len);
+                if (word_concurrence.count(word)) {
+                    auto& lines_indexes = word_concurrence[word];
+                    if (lines_indexes.empty() || lines_indexes.back().first != str_ind) {
+                        lines_indexes.push_back({str_ind, 0});
                     }
+                    lines_indexes.back().second++;
                 }
-
-                start_indx += len;
-                break;
             }
-        }
-        if (start_indx == text.size() || text[start_indx] == '\n') {
-            ordered_str.push_back({0, text.substr(start_str, start_indx - start_str)});
-            count_of_words.push_back(word_count);
-
-            start_str = start_indx + 1;
-            word_count = 0;
-            cur_str++;
+            char_ind += word_len;
         }
     }
 
-    for (auto const& [word, matches] : matches_str) {
-        if (matches.empty()) {
+    std::vector<double> tf_idf(splited_str.size());
+    for (auto const& [word, con] : word_concurrence) {
+        if (con.empty()) {
             continue;
         }
 
-        double cur_idf = std::log(static_cast<double>(ordered_str.size()) / static_cast<double>(matches.size()));
-        for (auto [str_indx, cur_word_cnt] : matches) {
-            double cur_tf = static_cast<double>(cur_word_cnt) / static_cast<double>(count_of_words[str_indx]);
-            ordered_str[str_indx].first = std::max(ordered_str[str_indx].first, cur_idf * cur_tf);
+        double idf = log(static_cast<double>(str_word_count.size()) / static_cast<double>(con.size()));
+        for (auto [line_i, word_c] : con) {
+            double tf = static_cast<double>(word_c) / static_cast<double>(str_word_count[line_i]);
+            tf_idf[line_i] += tf * idf;
         }
     }
 
-    sort(ordered_str.rbegin(), ordered_str.rend(),
-         [](std::pair<double, std::string_view> const& lhs, std::pair<double, std::string_view> const& rhs) {
-             return rhs.first - lhs.first > EPS;
-         });
+    std::vector<size_t> ordered_indx(splited_str.size());
+    std::iota(ordered_indx.begin(), ordered_indx.end(), 0);
+    std::sort(ordered_indx.begin(), ordered_indx.end(),
+              [&tf_idf](size_t lhs, size_t rhs) { return tf_idf[lhs] > tf_idf[rhs]; });
 
-    std::vector<std::string_view> top_k;
-    for (auto [tf_idf, str] : ordered_str) {
-        if (tf_idf < EPS || top_k.size() == results_count) {
+    std::vector<std::string_view> result(results_count);
+    for (size_t i = 0; i < results_count; i++) {
+        if (tf_idf[ordered_indx[i]] < EPS) {
             break;
         }
-        top_k.push_back(str);
+        result[i] = splited_str[ordered_indx[i]];
     }
-    return top_k;
+
+    return result;
 }

78dcdc245f3fef68556d08dd4e755ec0c73b6a18
Pavel Turaev <pnturaev@edu.hse.ru>
2024-10-17 19:46:21 +0300

    solve problem

--- tasks/stack/stack.cpp
+++ tasks/stack/stack.cpp
@@ -1,1 +1,45 @@
 #include "stack.h"
+
+Node::Node(int32_t value, Node* previous) : value(value), previous(previous) {
+}
+
+Stack::Stack() : head_(nullptr), size_(0) {
+}
+
+Stack::~Stack() {
+    while (head_) {
+        Node* next = head_->previous;
+        delete head_;
+        head_ = next;
+    }
+}
+
+void Stack::Push(int32_t value) {
+    head_ = new Node(value, head_);
+    size_++;
+}
+
+void Stack::Pop() {
+    Node* next_head = head_->previous;
+    delete head_;
+    head_ = next_head;
+    size_--;
+}
+
+int32_t Stack::Top() const {
+    return head_->value;
+}
+
+int32_t Stack::Size() const {
+    return size_;
+}
+
+void Stack::Clear() {
+    while (size_) {
+        Pop();
+    }
+}
+
+bool Stack::Empty() const {
+    return !size_;
+}
